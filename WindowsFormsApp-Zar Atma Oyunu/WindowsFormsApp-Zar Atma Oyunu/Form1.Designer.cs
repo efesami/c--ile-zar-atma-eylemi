757c8848381c05a3b3abdf1ddbe3fdedf920a6bc
Sami Efe ÜLGER <123024548+efesami@users.noreply.github.com>
2023-05-14 00:21:16 +0300

    c# ile iki adet zarın atılması

---
+++ WindowsFormsApp-Zar Atma Oyunu/WindowsFormsApp-Zar Atma Oyunu/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApp_Zar_Atma_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(327, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 203);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Stencil", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(154, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 72);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ZARLARI AT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(63, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.IndianRed;
+            this.ımageList1.Images.SetKeyName(0, "zar1.jpg");
+            this.ımageList1.Images.SetKeyName(1, "zar2.jpg");
+            this.ımageList1.Images.SetKeyName(2, "zar3.jpg");
+            this.ımageList1.Images.SetKeyName(3, "zar4.jpg");
+            this.ımageList1.Images.SetKeyName(4, "zar5.jpg");
+            this.ımageList1.Images.SetKeyName(5, "zar6.jpg");
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 35;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ZAR ATMA EYLEMİ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp-Zar Atma Oyunu/WindowsFormsApp-Zar Atma Oyunu/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Zar_Atma_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); // butona basınca timer ı baslat
+            button1.Enabled = false; // timer baslayınca(butona basınca) button1 adlı nesneyi pasif yap
+
+
+        }
+        int sayac;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Random random71 = new Random(); // random sınıfından random71 adında bir rastgele nesnesi ürettim.
+            int zar1, zar2; // int türünde 2 adet zar isimli değişkenler ürettm.
+            zar1 = random71.Next(0, 6); //  üretilecek olan sayı aralıgını belirttim. // next metodu üretebilecegimiz sayı aralığını belirtir.
+            zar2 = random71.Next(0, 6);  //  üretilecek olan sayı aralıgını belirttim. // burada 0 dahil 6 dahil değildir. çünkü eklediğim resimler dizinin indexi seklinde sıralanır. indexler 0. ile basladıgı için 0-6 yaptım max 5 i üretir demek..
+            pictureBox1.Image = ımageList1.Images[zar1]; // pıcturebox un imajına imagelistin ımageslerden birini rastgle atadık zar1 ve zar2 adındakı rastgele degiskenlerinden
+            pictureBox2.Image = ımageList1.Images[zar2];
+
+            sayac++;
+            if (sayac==30)
+            {
+                button1.Enabled = true; // sayac zamanına ulasınca button1 adlı nesneyi aktif yap
+                timer1.Stop(); // sayac zamanına ulasınca timeri durdur.
+                sayac = 0; // sayacı sıfırla
+
+            }
+        }
+    }
+}
